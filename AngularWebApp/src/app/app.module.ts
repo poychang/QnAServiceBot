c6cafb611cdb0f27984f7047749ba5efa7c57a32
Poy Chang <poypost@gmail.com>
2018-02-19 22:29:30 +0800

    建立登入頁

--- AngularWebApp/src/app/app.module.ts
+++ AngularWebApp/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {environment} from '../environments/environment';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {AuthModule} from './auth/auth.module';
 import {HomeModule} from './home/home.module';
 import {FooterComponent, HeaderComponent, SharedModule} from './shared';
 
@@ -20,6 +21,7 @@ const rootRouting: ModuleWithProviders = RouterModule.forRoot([]);
     BrowserAnimationsModule,
     AppRoutingModule,
     HomeModule,
+    AuthModule,
     rootRouting,
     SharedModule.forRoot(),
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
---
+++ AngularWebApp/src/app/auth/auth.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+
+import {UserService} from '../shared';
+
+@Component({
+  selector: 'app-auth-page',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
+})
+export class AuthComponent implements OnInit {
+  error: String = '';
+  authForm: FormGroup;
+
+  constructor(private router: Router, private userService: UserService, private fb: FormBuilder) {
+    this.authForm = this.fb.group(
+      { 'username': ['', Validators.required], 'password': ['', Validators.required] });
+  }
+
+  ngOnInit() {}
+
+  submitForm() {
+    this.error = '';
+
+    const credentials = this.authForm.value;
+    this.userService.attemptAuth(credentials)
+      .subscribe(data => this.router.navigateByUrl('/'), err => {
+        console.log(err);
+        this.error = '登入失敗，請重新登入';
+      });
+  }
+}
